6f56e9f064001a9c8eef02821483d5fa9d5fa607
zakaria <kharroubzakaria32@gmail.com>
2024-11-19 12:06:08 +0100

    save field

---
+++ src/main/java/com/example/citron/repository/FieldRepository.java
@@ -0,0 +1,9 @@
+package com.example.citron.repository;
+
+import com.example.citron.domaine.Field;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface FieldRepository extends JpaRepository<Field, UUID> {
+}
