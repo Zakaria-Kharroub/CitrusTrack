5cd9bef1ddc8cb7a07d8619886f89b0279a3c8b0
zakaria <kharroubzakaria32@gmail.com>
2024-11-19 10:46:43 +0100

    debrief

--- src/main/java/com/example/citron/domaine/Farm.java
+++ src/main/java/com/example/citron/domaine/Farm.java
@@ -1,7 +1,9 @@
 package com.example.citron.domaine;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.FutureOrPresent;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.*;
 
 import java.time.LocalDate;
@@ -18,12 +20,22 @@ public class Farm {
     @GeneratedValue(strategy = GenerationType.UUID)
     private UUID id;
 
+    @NotBlank
+    @NotNull
     String name;
 
+
+    @NotBlank
+    @NotNull
     String location;
 
+
+    @NotBlank
+    @NotNull
     Double totalArea;
 
+    @NotNull
+    @FutureOrPresent
     LocalDate creationDate;
 
     @OneToMany(mappedBy = "farm")
