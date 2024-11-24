3b08b649c693e986abb86107fe28db1f1c8d4555
zakaria <kharroubzakaria32@gmail.com>
2024-11-24 13:49:18 +0100

    final

--- src/main/java/com/example/citron/web/vm/mapper/HarvestMapper.java
+++ src/main/java/com/example/citron/web/vm/mapper/HarvestMapper.java
@@ -8,7 +8,6 @@ import com.example.citron.web.vm.HarvestVM;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 
-import java.util.List;
 
 @Mapper(componentModel = "spring")
 public interface HarvestMapper {
