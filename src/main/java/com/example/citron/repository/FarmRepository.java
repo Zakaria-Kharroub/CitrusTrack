4573efb0fc0a3ee33b4f811ea595216354c4380d
zakaria <kharroubzakaria32@gmail.com>
2024-11-26 16:42:48 +0100

    final

--- src/main/java/com/example/citron/repository/FarmRepository.java
+++ src/main/java/com/example/citron/repository/FarmRepository.java
@@ -3,9 +3,15 @@ package com.example.citron.repository;
 import com.example.citron.domaine.Farm;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.UUID;
 
 public interface FarmRepository extends JpaRepository<Farm, UUID>, JpaSpecificationExecutor<Farm> {
+
+//    @Query("SELECT DISTINCT f from Farm f"+
+//            "Join f.tree t"+
+//            "Join t.harvestDetail hd"
+//    );
 
 }
--- src/main/java/com/example/citron/service/dto/FarmDTO.java
+++ src/main/java/com/example/citron/service/dto/FarmDTO.java
@@ -1,11 +1,13 @@
 package com.example.citron.service.dto;
 
+import com.example.citron.domaine.Field;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.time.LocalDate;
+import java.util.List;
 import java.util.UUID;
 
 @Getter
@@ -18,4 +20,5 @@ public class FarmDTO {
     private String location;
     private Double totalArea;
     private LocalDate creationDate;
+    private List<Field> fields;
 }
